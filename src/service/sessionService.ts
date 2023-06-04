ef4bbb3253b9ed754a15d82c8a4698a90c37c65e
chetanguptaa <chetangupta123raj@gmail.com>
2023-06-04 22:39:02 +0530

    Revert "added logger.ts"
    This reverts commit 9cb1c760e1fff0847c53f6f574fd854ed77e1bd1.

--- src/service/sessionService.ts
+++
@@ -1,8 +0,0 @@
-import SessionModel from "../models/sessionModel";
-
-export async function createSession(userId: string, userAgent: string) {
-    const session = await SessionModel.create({
-        user: userId, userAgent
-    })
-    return session.toJSON();
-}
