dfd37b829b5bea628d47c1082881144737ef652e
chetanguptaa <chetangupta123raj@gmail.com>
2023-06-05 19:50:28 +0530

    updated routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,15 @@ import {Express, Request, Response} from 'express';
 import { createUserHandler } from './controllers/userController';
 import validateResource from './middleware/validateResource';
 import { createUserSchema } from './schemas/userSchema';
-
+import { createUserSessionHandler } from './controllers/sessionController';
+import { createSessionSchema } from './schemas/sessionSchema';
 
 const routes = (app: Express) => {
     app.get('/healthcheck', (req: Request, res: Response) => {
         res.sendStatus(200);
     })
     app.post('/api/users', validateResource(createUserSchema), createUserHandler)
+    app.post('/api/sessions', validateResource(createSessionSchema), createUserSessionHandler )
 }
 
 export default routes
