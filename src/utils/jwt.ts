5b1bfcb3fc6d271f3a0ed0429f7a529a2cb0cd79
chetanguptaa <chetangupta123raj@gmail.com>
2023-06-05 22:38:45 +0530

    updated jwt.ts

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import config from 'config';
 
 const privateKey = config.get<String>("privateKey");
+
 const publicKey = config.get<String>("publicKey");
 
 export function signJwt(
@@ -12,15 +13,14 @@ export function signJwt(
             privateKey, "base64"
         ).toString("ascii");
         return jwt.sign(object, signingKey, {
-            ...(options && options),
-            algorithm: 'RS256'
+            ...(options && options)
         })
 }
 export function verifyJwt(token: string) {
+    const signingKey = Buffer.from(
+        publicKey, "base64"
+    ).toString("ascii");
     try {
-        const signingKey = Buffer.from(
-            privateKey, "base64"
-        ).toString("ascii");
         const decoded = jwt.verify(token, signingKey);
         return {
             valid: true,
@@ -28,6 +28,7 @@ export function verifyJwt(token: string) {
             decoded
         }
     } catch(e: any) {
+        console.log(e);
         return {
             valid: false,
             expired: e.message === 'jwt expired',
