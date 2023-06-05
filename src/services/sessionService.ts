d9717d52827bca56c6e0d8b0a64ab167bbb01f3b
chetanguptaa <chetangupta123raj@gmail.com>
2023-06-05 23:28:03 +0530

    updated sessionService.ts

--- src/services/sessionService.ts
+++ src/services/sessionService.ts
@@ -1,7 +1,19 @@
-import SessionModel from '../models/sessionModel';
+import { FilterQuery } from 'mongoose';
+import SessionModel, { SessionDocument } from '../models/sessionModel';
 
 
 export async function createSession(userId: string, userAgent: string) {
     const session = await SessionModel.create({user: userId, userAgent});
     return session.toJSON();
+}
+
+export async function findSessions(query: FilterQuery<SessionDocument>) {
+    const session = await SessionModel.find(query).lean();
+    return session;
+}
+
+export async function deleteSession(query: FilterQuery<SessionDocument>) {
+    const session = await SessionModel.find(query).lean();
+    SessionModel.deleteOne(session);
+    return session;
 }
