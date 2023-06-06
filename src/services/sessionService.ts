58e624288588e33d382e877b37b210960a41035b
chetanguptaa <chetangupta123raj@gmail.com>
2023-06-06 19:38:45 +0530

    added sessionService.ts

--- src/services/sessionService.ts
+++ src/services/sessionService.ts
@@ -1,6 +1,9 @@
 import { FilterQuery, UpdateQuery } from 'mongoose';
 import SessionModel, { SessionDocument } from '../models/sessionModel';
-
+import { signJwt, verifyJwt } from '../utils/jwt';
+import { get } from 'lodash';
+import config from 'config';
+import {findUser} from './userService';
 
 export async function createSession(userId: string, userAgent: string) {
     const session = await SessionModel.create({user: userId, userAgent});
@@ -17,4 +20,25 @@ export async function updateSession(
     update: UpdateQuery<SessionDocument> 
     ) {
     return SessionModel.updateOne(query, update);
+}
+
+export async function reIssueAccessToken({
+    refreshToken,
+}: {
+    refreshToken: string,
+}) {
+    const { decoded } = verifyJwt(refreshToken, "refreshPublicKey");
+    if(!decoded || !get(decoded, "session")) return false;
+    const session = await SessionModel.findById(get(decoded, "session"));
+    if(!session || !session.valid) return false;
+    const user = await findUser({_id: session.user});
+    if(!user) return false;
+    const accessToken = signJwt(
+        {
+        ...user, session: session._id
+        },
+        "accessPrivateKey",
+        {expiresIn: config.get("accessTokenTimeToLive")}
+    );
+    return accessToken;
 }
